5d78d88401e66c8db298ec65beb4658ab5208e24
histjxg <13051813722@163.com>
2021-08-31 08:59:57 +0800

    1212

--- leave-service/src/main/java/ddd/leave/Application.java
+++ leave-service/src/main/java/ddd/leave/Application.java
@@ -1,8 +1,11 @@
 package ddd.leave;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableFeignClients
 @SpringBootApplication
