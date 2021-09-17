f392e7b72b2ccadb81e5652b52c8aa4784a9b10e
huangxiaogen <huangxiaogen@baijiahulian.com>
2021-09-17 19:21:07 +0800

    12

--- leave-service/src/main/java/profit/heima/cn/itcast/Constants.java
+++
@@ -1,6 +0,0 @@
-package profit.heima.cn.itcast;
-
-public final class Constants {
-
-    public static final String MP4_FULL_PATH = "/Users/huangxiaogen/work/sourceTree1/leave-sample-histjxg/leave-service/src/main/java/profit/heima/cn/itcast/n2/util/Sleeper.java";
-}
